4e39bf691cc44037b013f3ebbb542ed25d1790ea
Dirk-Jan Beijer <djjadebeijer@gmail.com>
2017-09-09 19:58:38 +0200

    Graaf werkt, yay!

--- Algoritmiek/oefening1/Algorithms/GraafAlgoritme.cs
+++ Algoritmiek/oefening1/Algorithms/GraafAlgoritme.cs
@@ -10,22 +10,88 @@ namespace Algorithms
     {
         public static Graaf graaf1 = new Graaf()
         {
-            new Route('A', 'B', 5),
-            new Route('A', 'D', 6),
-            new Route('D', 'E', 5),
-            new Route('D', 'F', 8),
-            new Route('F', 'C', 12),
-            new Route('B', 'E', 7),
-            new Route('B', 'C', 2),
-            new Route('E', 'C', 3)
+            new Link('A', 'B', 5),
+            new Link('A', 'D', 6),
+            new Link('D', 'E', 5),
+            new Link('D', 'F', 8),
+            new Link('F', 'C', 12),
+            new Link('B', 'E', 7),
+            new Link('B', 'C', 2),
+            new Link('E', 'C', 3)
         };
 
-        public static void Execute()
+        public static Graaf graaf1WithDirections = new Graaf()
         {
+            new Link('A', 'B', 5, Direction.AToB),
+            new Link('A', 'D', 6),
+            new Link('D', 'E', 5),
+            new Link('D', 'F', 8),
+            new Link('F', 'C', 12),
+            new Link('B', 'E', 7, Direction.BToA),
+            new Link('B', 'C', 2),
+            new Link('E', 'C', 3)
+        };
 
-        }
+        public static Graaf graaf2 = new Graaf()
+        {
+            #region outer ring
+            new Link('A', 'E', 4),
+            new Link('E', 'L', 3),
+            new Link('L', 'T', 18),
+            new Link('T', 'O', 3),
+            new Link('O', 'N', 5),
+            new Link('N', 'K', 2),
+            new Link('K', 'H', 3),
+            new Link('H', 'D', 2),
+            new Link('D', 'S', 11),
+            new Link('S', 'A', 1),
+            #endregion
+            #region outer nodes to inner nodes
+            new Link('A', 'B', 2),
+            new Link('A', 'F', 5),
 
-        public static void GetShortestRoute(Graaf graaf, char pointStart, char pointDestination)
+            new Link('E', 'F', 2),
+            new Link('E', 'I', 1),
+
+            new Link('L', 'I', 1),
+            new Link('L', 'J', 2),
+            new Link('L', 'M', 10),
+
+            new Link('T', 'M', 7),
+
+            new Link('O', 'M', 3),
+
+            new Link('N', 'M', 1),
+            new Link('N', 'J', 10),
+
+            new Link('K', 'J', 7),
+            new Link('K', 'G', 9),
+
+            new Link('H', 'G', 6),
+            new Link('H', 'C', 3),
+
+            new Link('D', 'C', 2),
+
+            new Link('S', 'C', 13),
+            new Link('S', 'B', 2),
+            #endregion
+
+            new Link('F', 'I', 3),
+            new Link('I', 'J', 1),
+            new Link('J', 'G', 3),
+            new Link('G', 'C', 1),
+            new Link('C', 'B', 9),
+            new Link('B', 'F', 3),
+
+            new Link('J', 'M', 9),
+            new Link('I', 'G', 2),
+            new Link('F', 'G', 1),
+            new Link('B', 'G', 5)
+
+        };
+
+
+        public static List<Route> GetPossibleRoutes(Graaf graaf, char pointStart, char pointDestination)
         {
             // alle routes
             // welke nodes zitten aan A?
@@ -33,9 +99,6 @@ namespace Algorithms
             // welke nodes zitten aan A2 en hebben we nog niet gehad?
             // ... ect
 
-            // Vervolgens sorteren
-
-            //var routes = new List<List<char>>();
             var startNode = new Node(pointStart);
             var stopNode = new Node(pointDestination);
             var passedThrough = new List<Node>()
@@ -45,7 +108,110 @@ namespace Algorithms
 
             startNode.ConnectedNodes = GetConnectedNodes(graaf, startNode, stopNode, passedThrough);
 
+            //var paths = GetConnectedNodesPathsAsString(startNode);
+            //Console.WriteLine("Possible Paths:");
+            //foreach (var path in paths)
+            //{
+            //    Console.WriteLine(path);
+            //}
 
+            var routes = GetConnectedNodeRoutes(startNode, graaf);
+
+            var routesWithEnding = new List<Route>();
+            foreach (var route in routes)
+            {
+                var lastLink = route.Links[route.Links.Count - 1];
+                if (lastLink.PointA.Id == pointDestination || lastLink.PointB.Id == pointDestination)
+                    routesWithEnding.Add(route);
+            }
+
+            return routesWithEnding;
+        }
+
+        public static Route GetShortestRoute(List<Route> routes)
+        {
+            BubleSortRoutes(routes);
+            return routes[0];
+        }
+
+        public static List<Route> Get10ShortestRoutes(List<Route> routes)
+        {
+            BubleSortRoutes(routes);
+
+            var temp = new List<Route>();
+            for (int i = 0; i < (routes.Count >= 10 ? 10 : routes.Count); i++)
+            {
+                temp.Add(routes[i]);
+            }
+
+            return temp;
+        }
+
+        private static List<string> GetConnectedNodesPathsAsString(Node node)
+        {
+            var list = new List<string>();
+            var curNode = node.Id.ToString();
+            if (node.ConnectedNodes != null && node.ConnectedNodes.Count > 0)   // If the GetConnectedNodes algorithm reaches the specified endNode, connected nodes will be null. If it finds a node without connection, path to nowhere, it's not null
+            {
+                foreach (var conNode in node.ConnectedNodes)
+                {
+                    var childPaths = GetConnectedNodesPathsAsString(conNode);
+                    foreach (var childPath in childPaths)
+                    {
+                        list.Add(curNode + " " + childPath);
+                    }
+                }
+            }
+            else
+            {
+                list.Add(curNode);
+            }
+            return list;
+        }
+
+        private static List<Route> GetConnectedNodeRoutes(Node node, Graaf graaf)
+        {
+            var list = new List<Route>();
+
+            if (node.ConnectedNodes != null && node.ConnectedNodes.Count > 0)
+            {
+                foreach (var conNode in node.ConnectedNodes)
+                {
+                    // We have a link --> add it to the route
+                    // Check if this node has more links
+                    var curLink = graaf.GetLink(node.Id, conNode.Id);
+                    //var temp = new Route();
+                    //temp.AddLink(curLink);
+
+                    var subRoutes = GetConnectedNodeRoutes(conNode, graaf);
+                    if (subRoutes.Count > 0)
+                    {
+                        foreach (var subRoute in subRoutes)
+                        {
+                            var temp = new Route();
+                            temp.AddLink(curLink);
+                            foreach (var link in subRoute.Links)
+                            {
+                                temp.AddLink(link);
+                            }
+                            list.Add(temp);
+                        }
+                    }
+                    else
+                    {
+                        // No more subroutes, only add the current link
+                        var temp = new Route();                             // Should perhaps use a List<Link> in stead of Route to save memory
+                        temp.AddLink(curLink);
+                        list.Add(temp);
+                    }
+                }
+            }
+            else
+            {
+                // If there are no more connected nodes, there're no new links
+            }
+
+            return list;
         }
 
         private static List<Node> GetConnectedNodes(Graaf graaf, Node nodeStart, Node nodeStop, List<Node> nodesPassedThrough)
@@ -55,30 +221,31 @@ namespace Algorithms
             foreach (var route in graaf)
             {
                 // Check if route has nodeStart in it
-                if (route.PointA.Id == nodeStart.Id)
+                if (route.PointA.Id == nodeStart.Id && route.Direction != Direction.BToA)
                 {
                     if (!IsNodeInList(nodesPassedThrough, route.PointB))
                     {
-                        connectedNodes.Add(route.PointB);
+                        connectedNodes.Add(new Node(route.PointB.Id));  // Was a reference problem --> connectedNodes.Add(route.PointB); --> Takes point from graaf, does it again later and changes it what causes the bug
                     }
                 }
-                else if (route.PointB.Id == nodeStart.Id)
+                else if (route.PointB.Id == nodeStart.Id && route.Direction != Direction.AToB)
                 {
                     if (!IsNodeInList(nodesPassedThrough, route.PointA))
                     {
-                        connectedNodes.Add(route.PointA);
+                        connectedNodes.Add(new Node(route.PointA.Id));
                     }
                 }
             }
 
             foreach (var item in connectedNodes)
             {
-                if(item.Id != nodeStop.Id)
+                if (item.Id != nodeStop.Id)
                 {
                     var temp = new List<Node>();
                     temp.AddRange(nodesPassedThrough);
                     temp.Add(item);
 
+                    item.IgnoredNodes = temp;
                     item.ConnectedNodes = GetConnectedNodes(graaf, item, nodeStop, temp);
                 }
             }
@@ -95,47 +262,137 @@ namespace Algorithms
             return false;
         }
 
-        //public static List<Route> GetRoutes(Graaf graaf, char pointA, char pointB, int connections)
-        //{
-        //    var list = new List<Route>();
-
-
-
-        //    return list;
-        //}
-
-        //private static List<Route> GetRoutesConnectedToNodes(Graaf graaf, char pointA, char pointB)
-        //{
-        //    var list = new List<Route>();
-
-        //    foreach (var r in graaf)
-        //    {
-        //        if (r.PointA == pointA || r.PointA == pointB || r.PointB == pointA || r.PointB == pointB)
-        //            list.Add(r);
-        //    }
-
-        //    return list;
-        //}
+        public static void BubleSortRoutes(List<Route> routes)
+        {
+            var swapped = true;
+            int iteration = 0;
+            while (swapped)
+            {
+                swapped = false;
+                for (int i = 1; i < (routes.Count - iteration); i++)
+                {
+                    if (routes[i].Distance < routes[i - 1].Distance)
+                    {
+                        var temp = routes[i];
+                        routes[i] = routes[i - 1];
+                        routes[i - 1] = temp;
+                        swapped = true;
+                    }
+                }
+                iteration++;
+            }
+        }
     }
 
-    public class Graaf : List<Route>
+    public class Graaf : List<Link>
     {
-        //public List<Route> Routes { get; set; }
+        public Link GetLink(char pointFrom, char pointTo)
+        {
+            foreach (var link in this)
+            {
+                if (((link.PointA.Id == pointFrom && link.PointB.Id == pointTo && link.Direction != Direction.BToA) || (link.PointA.Id == pointTo && link.PointB.Id == pointFrom && link.Direction != Direction.AToB)))
+                {
+                    return link;
+                }
+            }
+            return null;
+        }
     }
 
     public class Route
+    {
+        public List<Link> Links { get; }
+        public int Distance { get; private set; }
+
+        public Route()
+        {
+            Links = new List<Link>();
+            Distance = 0;
+        }
+        public void AddLink(Link link)
+        {
+            Links.Add(link);
+            Distance += link.Distance;
+        }
+
+        public override string ToString()
+        {
+            string str = $"[{Distance}]".PadRight(5, ' ');
+            foreach (var link in Links)
+            {
+                str += link.ToString() + " ";
+            }
+            return str;
+        }
+
+        public string ToShortString()
+        {
+            string str = $"({Distance})".PadRight(5, ' ');
+
+            if (Links.Count == 1)
+            {
+                str += $"{Links[0].PointA.Id} {Links[0].PointB.Id}";
+            }
+            else
+            {
+                for (int i = 0; i < (Links.Count - 1); i++)
+                {
+                    var curLink = Links[i];
+                    var nexLink = Links[i + 1];
+
+                    if (curLink.PointA.Id == nexLink.PointA.Id || curLink.PointA.Id == nexLink.PointB.Id)
+                    {
+                        if (i == 0)
+                            str += curLink.PointB.Id + " ";
+
+                        str += curLink.PointA.Id + " ";
+
+                        if (i == Links.Count - 2)
+                            str += curLink.PointA.Id == nexLink.PointA.Id ? nexLink.PointB.Id : nexLink.PointA.Id;
+                    }
+                    else if (curLink.PointB.Id == nexLink.PointA.Id || curLink.PointB.Id == nexLink.PointB.Id)
+                    {
+                        if (i == 0)
+                            str += curLink.PointA.Id + " ";
+
+                        str += curLink.PointB.Id + " ";
+
+                        if (i == Links.Count - 2)
+                            str += curLink.PointB.Id == nexLink.PointA.Id ? nexLink.PointB.Id : nexLink.PointA.Id;
+                    }
+                }
+            }
+
+
+            return str;
+        }
+    }
+
+    public class Link
     {
         public Node PointA { get; set; }
         public Node PointB { get; set; }
         public int Distance { get; set; }
         public Direction Direction { get; set; }
 
-        public Route(char pointA, char pointB, int distance)
+        public Link(char pointA, char pointB, int distance)
         {
             PointA = new Node(pointA);
             PointB = new Node(pointB);
             Distance = distance;
             Direction = Direction.Both;
+        }
+
+        public Link(char pointA, char pointB, int distance, Direction direction) : this(pointA, pointB, distance)
+        {
+            Direction = direction;
+        }
+
+        public override string ToString()
+        {
+            var leftDirection = (Direction == Direction.Both || Direction == Direction.BToA) ? "<" : "-";
+            var rightDirection = (Direction == Direction.Both || Direction == Direction.AToB) ? ">" : "-";
+            return $"{PointA}-{leftDirection}{Distance.ToString().PadLeft(3, '_')}{rightDirection}-{PointB}";
         }
     }
 
@@ -143,20 +400,16 @@ namespace Algorithms
     {
         public char Id { get; set; }
         public List<Node> ConnectedNodes { get; set; }
+        public List<Node> IgnoredNodes { get; set; }
 
         public Node(char id)
         {
             Id = id;
         }
-        //public override bool Equals(object obj)
-        //{
-        //    if (((Node)obj).Id == Id)
-        //        return true;
-        //    return false;
-        //}
+
         public override string ToString()
         {
-            return $"[{Id}]";
+            return $"({Id})";
         }
     }
 
